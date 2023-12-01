fb09d10266f3cb815ed1734a9cbb2783f971cb34
FoxRio <davis.krasts@gmail.com>
2023-12-01 22:57:49 +0200

    init: initial commit

---
+++ dk22052_main.py
@@ -0,0 +1,8 @@
+# This is a simple calculation program
+# created to demonstrate version control.
+from dk22052_libadd import *
+from dk22052_libmult import *
+a = 13
+b = 5
+print(plus(a,b))
+print(mult(a,b))
